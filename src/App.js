e880101132bcb6d77b8ad4f1c80749f1745c8253
Anik Thomas <aniek.sw@gmail.com>
2021-04-11 20:21:01 -0600

    setup the router

--- src/App.js
+++ src/App.js
@@ -3,38 +3,43 @@ import './App.css';
 import Axios from 'axios';
 import TopNav from './components/TopNav';
 import Home from './components/Home';
+import Footer from './components/Footer';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 
-class App extends Component{
+class App extends Component {
 
   constructor(props){
     super(props);
-    this.state = {vehicleData: null}
+    this.state = { vehicleData: null};
   }
+
   componentDidMount(){
-    Axios.get("http://localhost:3002/vehicles")
-      .then(res => {
+    Axios.get("http://localhost:3001/vehicles")
+      .then( res => {
         console.log(res.data);
         this.setState({ vehicleData: res.data});
       })
       .catch( err => console.log(err));
   }
 
-  render(){
+  render() {
     if(this.state.vehicleData){
-      return (
-        <div className="App">
-          <TopNav/>
-          <Home />
-        </div>
-      );
-    }else{
       return(
-        <h4>Loading Data...</h4>
+        <Router>
+          <div className="App">
+            <TopNav />
+              <div className="contentAread">
+                <Route exact path='/' component={Home} />
+              </div>
+            <Footer />
+          </div>
+        </Router>
       );
+    } else {
+      return <h4>Loading Data...</h4>;
     }
   }
 }
-
 
 export default App;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import './Home.css';
 import SiteCarousel from '../SiteCarousel';
+import VehicleBrowser from '../VehicleBrowser';
 
 class Home extends React.Component{
-    
     constructor(props){
         super(props);
     }
     render(){
         return (
             <div>
-                <SiteCarousel/>
+                <p>Test</p>
+                <SiteCarousel />
+                <VehicleBrowser />
             </div>
-        )
+        );
     }
 }
 
