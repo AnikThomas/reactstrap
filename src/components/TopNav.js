8bc327bd16cbdae766f98a1c24979a1171c4566d
Anik Thomas <aniek.sw@gmail.com>
2021-04-11 18:51:09 -0600

    2nd commit

---
+++ src/components/TopNav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '/Users/anikthomas/Desktop/reactstrap/src/components/TopNav/tnav.css';
+
+class TopNav extends React.Component{
+    constructor(props){
+        super(props);
+
+    }
+    render(){
+        return(<p>This is the navbar</p>);
+    }
+}
+
+export default TopNav;
